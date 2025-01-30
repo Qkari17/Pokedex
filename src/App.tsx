3bf2101717589b95bc2b1975ec5d849a697a2a45
Dawid <qkari177@gmail.com>
2025-01-30 17:17:41 +0100

    Setup

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import { Button } from "./components/Button";
+
+function App() {
+  return (
+    <>
+      <link href="/dist/styles.css" rel="stylesheet"></link>
+      <h1 className=" text-red-400 text-3xl font-bold underline ">Hello world!</h1>
+      <Button></Button>
+    </>
+  );
+}
+
+export default App;
