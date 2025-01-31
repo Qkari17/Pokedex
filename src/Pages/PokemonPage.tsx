8b0cc2d719bf82ae0250f3ade3f0798c39825f55
Dawid <qkari177@gmail.com>
2025-01-31 02:23:17 +0100

    Pokedex placeholder added

--- src/Pages/PokemonPage.tsx
+++ src/Pages/PokemonPage.tsx
@@ -1,5 +1,9 @@
 import { useQuery } from "@tanstack/react-query";
-import { capitalizeFirstLetter, fetchPokemons, PokemonDetails } from "../services/function";
+import {
+  capitalizeFirstLetter,
+  fetchPokemons,
+  PokemonDetails,
+} from "../services/function";
 
 export const PokemonPage = () => {
   const { data, error, isLoading } = useQuery<PokemonDetails[]>({
@@ -11,20 +15,28 @@ export const PokemonPage = () => {
   if (error) return <p>Błąd: {error.message}</p>;
 
   return (
-    <div >
+    <div>
       <ul className="grid grid-cols-3 ">
         {data?.map((pokemon) => (
           <li className="m-auto" key={pokemon.id}>
             <div className="border flex flex-col items-center w-40 h-40 gap-1">
-             <img className="h-1/2 w-1/2"
-              src={pokemon.sprites.front_default} alt={pokemon.name} />
-            <p>{pokemon.id}  <strong>{capitalizeFirstLetter(pokemon.name)}</strong></p>
-           
-            <div className="flex gap-2">
-              {pokemon.types.map((t, index) => (
-                <div className="border py-0.5 px-2" key={index}>{capitalizeFirstLetter(t.type.name)}</div>
-              ))}
-            </div>
+              <img
+                className="h-1/2 w-1/2"
+                src={pokemon.sprites.front_default}
+                alt={pokemon.name}
+              />
+              <p>
+                {pokemon.id}{" "}
+                <strong>{capitalizeFirstLetter(pokemon.name)}</strong>
+              </p>
+
+              <div className="flex gap-2">
+                {pokemon.types.map((t, index) => (
+                  <div className="border py-0.5 px-2" key={index}>
+                    {capitalizeFirstLetter(t.type.name)}
+                  </div>
+                ))}
+              </div>
             </div>
           </li>
         ))}
--- src/components/Productt/PokedexBox.tsx
+++ src/components/Productt/PokedexBox.tsx
@@ -3,8 +3,10 @@ import { PokemonPage } from "../../Pages/PokemonPage";
 export const PokedexBox = () => {
   return (
     <div>
-      <div className="flex justify-center m-10" >
-        <PokemonPage ></PokemonPage>
+      <div className="bg bg-red-600 flex m-auto pt-10 pb-72  w-[40rem] mt-5">
+        <div className="flex justify-center h-[31rem] w-[32rem] m-auto border overflow-y-scroll bg-white">
+          <PokemonPage></PokemonPage>
+        </div>
       </div>
     </div>
   );
